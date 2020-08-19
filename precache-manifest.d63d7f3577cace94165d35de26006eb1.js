0b1d08b5fbdf1697f18d5ecc9d0cb6c5a0a3bbe6
Vivek Raman <vr.ac4bf@live.com>
2020-08-19 20:38:55 +0530

    Updates

--- precache-manifest.d63d7f3577cace94165d35de26006eb1.js
+++ precache-manifest.d63d7f3577cace94165d35de26006eb1.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "85df933d8805933c4914cb86a5367288",
+    "revision": "85666581154e81050121700e7370a9d8",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "37887a796181ae5ba8bf",
-    "url": "/portfolio/static/css/main.cffc36b3.chunk.css"
+    "revision": "7639c14f98a406fdc72c",
+    "url": "/portfolio/static/css/main.c48ff472.chunk.css"
   },
   {
-    "revision": "646456aeabb8f83967c3",
-    "url": "/portfolio/static/js/2.2a721fd2.chunk.js"
+    "revision": "a2d27ced1288d3b9a7ed",
+    "url": "/portfolio/static/js/2.aa6dc3c3.chunk.js"
   },
   {
-    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/portfolio/static/js/2.2a721fd2.chunk.js.LICENSE.txt"
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.aa6dc3c3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "37887a796181ae5ba8bf",
-    "url": "/portfolio/static/js/main.8a28fb97.chunk.js"
+    "revision": "7639c14f98a406fdc72c",
+    "url": "/portfolio/static/js/main.8b522f05.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1e2ebe64d522fcc042ab86a17d64e6e4.js"
+  "/portfolio/precache-manifest.d63d7f3577cace94165d35de26006eb1.js"
 );
 
 self.addEventListener('message', (event) => {
