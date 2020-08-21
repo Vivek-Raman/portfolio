7627be867a7725d1f09df07a81cce5e5addf3b76
Vivek Raman <vr.ac4bf@live.com>
2020-08-21 14:30:40 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.62819be413542ebbd9d78ea24b561ed9.js"
+  "/portfolio/precache-manifest.a5c8f0d2b52b0134cc7b0195363982c8.js"
 );
 
 self.addEventListener('message', (event) => {
