20bb2dcaab307ac78e46eeaba01c7debeac8697f
Vivek Raman <vr.ac4bf@live.com>
2020-08-22 10:31:43 +0530

    Updates

---
+++ precache-manifest.796f7f9d4790f30047f9dd47455a6c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aada169fc09f075ef600d092b5ab208",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a17f1ae259ff53f0ad5d",
+    "url": "/portfolio/static/css/main.95d3f012.chunk.css"
+  },
+  {
+    "revision": "bd26767147e718cbdce5",
+    "url": "/portfolio/static/js/2.203164b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.203164b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17f1ae259ff53f0ad5d",
+    "url": "/portfolio/static/js/main.00813da3.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
